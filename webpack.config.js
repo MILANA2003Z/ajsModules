c45f9e1095d98a57d40d5f32b6036adbd3cb0b4a
MILANA2003Z <96334845+MILANA2003Z@users.noreply.github.com>
2025-11-05 22:33:45 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,40 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    //entry: './src/index.js' - если надо указать другую точку входа
-    module: {
-      rules: [
-        {
-          test: /\.txt$/,
-          loader: 'raw-loader'
-        },
-        {
-          test: /\.css$/,
-          use: [
-            MiniCSSExtractPlugin.loader,
-            'css-loader'
-          ]
-        }
-      ]
+  entry: './src/index.js', // точка входа
+  output: {
+    filename: 'bundle.js', // итоговый файл
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // очищает dist перед сборкой
+  },
+  mode: 'development', // режим по умолчанию
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCSSExtractPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        // filename: 'main.html'    - можем переименовать фаил таким образом
-      }),
-      new MiniCSSExtractPlugin()
-    ]
-}
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+};
+
