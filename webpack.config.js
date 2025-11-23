1ac63d88ed0d36be1b790dca72b5a9edd327eb18
MILANA2003Z <96334845+MILANA2003Z@users.noreply.github.com>
2025-11-23 11:36:25 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,56 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  // Точка входа
   entry: "./src/index.js",
+
+  // Вывод сборки
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "main.js",
+    clean: true, // очищает dist перед сборкой
   },
+
   module: {
     rules: [
+      // JS через Babel
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: "babel-loader",
       },
+      // HTML
       {
         test: /\.html$/,
-        use: "html-loader"
+        use: "html-loader",
       },
+      // CSS
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
+
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "index.html"
+      filename: "index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css"
-    })
-  ]
+      filename: "[name].css",
+    }),
+  ],
+
+  // devServer для npm start
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+
+  mode: "development", // по умолчанию
 };
